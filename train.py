a0ecd1611f5dfb0da3fd1b3cde3fc74a030d0656
Diego Porres <daporres@ufm.edu>
2021-04-25 18:36:17 +0200

    Forgot to add to command line/click option

--- train.py
+++ train.py
@@ -438,7 +438,7 @@ class CommaSeparatedList(click.ParamType):
 @click.option('--mirror-y', help='Enable dataset y=flips [default: false]', type=bool, metavar='BOOL')
 
 # Base config.
-@click.option('--cfg', help='Base config [default: auto]', type=click.Choice(['auto', 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar']))
+@click.option('--cfg', help='Base config [default: auto]', type=click.Choice(['auto', 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar', '24gb-2gpu']))
 @click.option('--gamma', help='Override R1 gamma', type=float)
 @click.option('--kimg', help='Override training duration', type=int, metavar='INT')
 @click.option('--batch', help='Override batch size', type=int, metavar='INT')
