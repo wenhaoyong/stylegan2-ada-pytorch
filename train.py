08378e1d2ac5db41b62829fc65e68fa990ad834d
Diego Porres <daporres@ufm.edu>
2021-04-28 15:17:19 +0200

    Forgot to add new configs to the parser

--- train.py
+++ train.py
@@ -448,7 +448,7 @@ class CommaSeparatedList(click.ParamType):
 @click.option('--mirror-y', help='Enable dataset y=flips [default: false]', type=bool, metavar='BOOL')
 
 # Base config.
-@click.option('--cfg', help='Base config [default: auto]', type=click.Choice(['auto', 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar', '24gb-2gpu']))
+@click.option('--cfg', help='Base config [default: auto]', type=click.Choice(['auto', 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar', '24gb-2gpu', '24gb-4gpu', '24gb-2gpu-cplx', '24gb-4gpu-cplx']))
 @click.option('--gamma', help='Override R1 gamma', type=float)
 @click.option('--kimg', help='Override training duration', type=int, metavar='INT')
 @click.option('--batch', help='Override batch size', type=int, metavar='INT')
